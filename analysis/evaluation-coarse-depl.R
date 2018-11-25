57ee5a94dcc9f8d6477bdea016081c3ca2ff58dc
Michael Hahn <mhahn29@gmail.com>
2018-11-25 15:10:20 -0800

    analysis of optimization

---
+++ analysis/evaluation-coarse-depl.R
@@ -0,0 +1,60 @@
+
+data = read.csv("CS_SCR/deps/manual_output_funchead_coarse_depl/auto-summary-lstm.tsv", sep="\t")# %>% rename(Quality=AverageLength)
+
+#/u/scr/mhahn/deps/manual_output_funchead_coarse_depl/
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+dryer_greenberg_fine = data
+
+
+
+languages = read.csv("languages.tsv", sep="\t")
+dryer_greenberg_fine  = merge(dryer_greenberg_fine, languages, by=c("Language"), all.x=TRUE)
+
+
+library("brms")
+
+#options(mc.cores = parallel::detectCores())
+rstan_options(auto_write = TRUE)
+
+dependency = "nmod"
+
+getCorrPair = function(dependency) {
+   corr_pair = dryer_greenberg_fine %>% filter((Dependency == dependency) | (Dependency == "obj"))
+   corr_pair = unique(corr_pair %>% select(Family, Language, FileName, Dependency, DH_Weight, ModelName)) %>% spread(Dependency, DH_Weight)
+   corr_pair$correlator = corr_pair[[dependency]]
+   corr_pair = corr_pair %>% mutate(correlator_s = pmax(0,sign(correlator)), obj_s=pmax(0,sign(obj)))
+   return(corr_pair)
+}
+
+corr_pair = getCorrPair("nmod")
+model2 = brm(correlator_s ~ obj_s + (1+obj_s|Family) + (1+obj_s|Language), family="bernoulli", data=corr_pair)
+
+dependencies = c("acl", "advcl", "advmod", "amod", "appos", "aux", "ccomp", "compound", "conj", "csubj", "dep", "det", "discourse", "dislocated", "expl", "fixed", "flat", "goeswith", "iobj", "lifted_case", "lifted_cc", "lifted_cop", "lifted_mark", "list", "nmod", "nsubj", "nummod", "obl", "orphan", "parataxis", "reparandum", "vocative", "xcomp")
+
+
+
+sink("results-depl.tsv")
+cat("")
+sink()
+
+cat(paste("dependency", "satisfiedFraction", "posteriorMean", "posteriorSD", "posteriorOpposite", sep="\t"), file="results-depl.tsv", append=TRUE, sep="\n")
+
+
+for(dependency in dependencies) {
+   corr_pair = getCorrPair(dependency)
+   model2 = update(model2, newdata=corr_pair, iter=4000)
+   summary(model2)
+   
+   samples = posterior_samples(model2, "b_obj_s")[,]
+   posteriorOpposite = ecdf(samples)(0.0)
+   posteriorMean = mean(samples)
+   posteriorSD = sd(samples)
+   satisfiedFraction = mean((corr_pair$correlator_s == corr_pair$obj_s), na.rm=TRUE)
+   cat(paste(dependency, satisfiedFraction, posteriorMean, posteriorSD, posteriorOpposite, sep="\t"), file="results-depl.tsv", append=TRUE, sep="\n")
+}
+
+
