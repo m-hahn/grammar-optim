5bc79a82889f129da0c8a124e2ede5299e7e0a48
Michael Hahn <mhahn29@gmail.com>
2019-01-19 13:14:51 -0800

    rewired joint evaluation

--- results/correlations/evaluation-coarse-langmod-best-prevalence.R
+++ results/correlations/evaluation-coarse-langmod-best-prevalence.R
@@ -1,6 +1,6 @@
 
 # _final/
-data = read.csv("CS_SCR/deps/manual_output_funchead_langmod_coarse_best_balanced/auto-summary-lstm.tsv", sep="\t")# %>% rename(Quality=AverageLength)
+data = read.csv("../../grammars/manual_output_funchead_langmod_coarse_best_balanced/auto-summary-lstm.tsv", sep="\t")# %>% rename(Quality=AverageLength)
 
 library(forcats)
 library(dplyr)
@@ -16,7 +16,7 @@ dryer_greenberg_fine = data
 
 
 
-languages = read.csv("languages-iso_codes.tsv")
+languages = read.csv("../languages/languages-iso_codes.tsv")
 dryer_greenberg_fine  = merge(dryer_greenberg_fine, languages, by=c("Language"), all.x=TRUE)
 
 
@@ -48,18 +48,18 @@ model3 = brm(agree ~ (1|Family) + (1|Language), family="bernoulli", data=corr_pa
 
 
 # The full set of UD relations
-#dependencies = c("acl", "advcl", "advmod", "amod", "appos", "aux", "ccomp", "compound", "conj", "csubj", "dep", "det", "discourse", "dislocated", "expl", "fixed", "flat", "goeswith", "iobj", "lifted_case", "lifted_cc", "lifted_cop", "lifted_mark", "list", "nmod", "nsubj", "nummod", "obl", "orphan", "parataxis", "reparandum", "vocative", "xcomp")
+dependencies = c("acl", "advcl", "advmod", "amod", "appos", "aux", "ccomp", "compound", "conj", "csubj", "dep", "det", "discourse", "dislocated", "expl", "fixed", "flat", "goeswith", "iobj", "lifted_case", "lifted_cc", "lifted_cop", "lifted_mark", "list", "nmod", "nsubj", "nummod", "obl", "orphan", "parataxis", "reparandum", "vocative", "xcomp")
 
 # Relations formalizing Dryer's correlations
-dependencies = c("acl", "advmod", "aux", "lifted_case", "lifted_cop", "lifted_mark", "nmod", "nsubj", "obl", "xcomp")
+#dependencies = c("acl", "advmod", "aux", "lifted_case", "lifted_cop", "lifted_mark", "nmod", "nsubj", "obl", "xcomp")
 
 
-
-sink("results-prevalence-langmod-best.tsv")
+outpath = "output/results-prevalence-langmod-best.tsv"
+sink(outpath)
 cat("")
 sink()
 
-cat(paste("dependency", "satisfiedFraction", "posteriorMean", "posteriorSD", "posteriorOpposite", sep="\t"), file="results-prevalence-langmod-best.tsv", append=TRUE, sep="\n")
+cat(paste("dependency", "satisfiedFraction", "posteriorMean", "posteriorSD", "posteriorOpposite", sep="\t"), file=outpath, append=TRUE, sep="\n")
 
 
 for(dependency in dependencies) {
@@ -72,7 +72,7 @@ for(dependency in dependencies) {
    posteriorMean = mean(samples)
    posteriorSD = sd(samples)
    satisfiedFraction = mean((corr_pair$correlator_s == corr_pair$obj_s), na.rm=TRUE)
-   cat(paste(dependency, satisfiedFraction, posteriorMean, posteriorSD, posteriorOpposite, sep="\t"), file="results-prevalence-langmod-best.tsv", append=TRUE, sep="\n")
+   cat(paste(dependency, satisfiedFraction, posteriorMean, posteriorSD, posteriorOpposite, sep="\t"), file=outpath, append=TRUE, sep="\n")
 }
 
 
