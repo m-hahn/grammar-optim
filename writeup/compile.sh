f1992b334f775d79de9c1ec8586491adede46795
Michael Hahn <mhahn29@gmail.com>
2019-05-30 21:20:22 -0700

    CODE

---
+++ writeup/compile.sh
@@ -0,0 +1,12 @@
+pdflatex pnas-word-order-si.tex
+bibtex pnas-word-order-si
+pdflatex si-table-perrel-1.tex
+pdflatex si-table-perrel-2.tex
+pdflatex si-table-perrel-3.tex
+
+pdftoppm si-table-perrel-1.pdf si-table-perrel-1 -png
+pdftoppm si-table-perrel-2.pdf si-table-perrel-2 -png
+pdftoppm si-table-perrel-3.pdf si-table-perrel-3 -png
+
+pdflatex pnas-word-order-si.tex
+
