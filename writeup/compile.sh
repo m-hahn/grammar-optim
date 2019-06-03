c945fcc182b0025f079b9b447eb6d96dcb85233f
m-hahn <mhahn29@gmail.com>
2019-06-02 22:17:24 -0700

    SI

--- writeup/compile.sh
+++ writeup/compile.sh
@@ -1,11 +1,15 @@
 pdflatex pnas-word-order-si.tex
 bibtex pnas-word-order-si
 pdflatex si-table-perrel-1.tex
-pdflatex si-table-perrel-2.tex
+pdflatex si-table-perrel-1a.tex
+pdflatex si-table-perrel-1b.tex
+pdflatex si-table-perrel-2a.tex
 pdflatex si-table-perrel-3.tex
 
 pdftoppm si-table-perrel-1.pdf si-table-perrel-1 -png
-pdftoppm si-table-perrel-2.pdf si-table-perrel-2 -png
+pdftoppm si-table-perrel-1a.pdf si-table-perrel-1a -png
+pdftoppm si-table-perrel-1b.pdf si-table-perrel-1b -png
+pdftoppm si-table-perrel-2a.pdf si-table-perrel-2a -png
 pdftoppm si-table-perrel-3.pdf si-table-perrel-3 -png
 
 pdflatex pnas-word-order-si.tex
