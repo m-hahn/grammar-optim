aefed936fae3dcb25026bde837fad57143887cb5
Michael Hahn <mhahn29@gmail.com>
2019-09-13 17:31:11 -0400

    code

--- results/plane/analyze_pareto_optimality/bayesianAnalysisBaseline.R
+++ results/plane/analyze_pareto_optimality/bayesianAnalysisBaseline.R
@@ -1,10 +1,11 @@
 data = read.csv("pareto-data.tsv")
 
-
 library(dplyr)
 library(tidyr)
 
-data2 = data %>% filter(Language == "English")
+language = "Korean"
+
+data2 = data %>% filter(Language == language)
 
 surp = data2$Surp
 pars = data2$Pars
@@ -22,40 +23,66 @@ X = cbind(surp, pars)
 X = X[!is.na(X[,1]),]
 X = X[!is.na(X[,2]),]
 
-
-
-
 library(dirichletprocess)
 dp = DirichletProcessMvnormal(X)
 dp = Fit(dp, 2000)
 
+bestLambdas = c()
+bestQuantiles = c()
+
 i = 1000
 for(i in (1000:2000)) {
- weight = dp$weightsChain[[i]]
- mu = dp$clusterParametersChain[[i]]$mu
- sig = dp$clusterParametersChain[[i]]$sig
+   weight = dp$weightsChain[[i]]
+   mu = dp$clusterParametersChain[[i]]$mu
+   sig = dp$clusterParametersChain[[i]]$sig
+  
+   # calculate the density assigned to better efficiency values, for each lambda
+  
+   # shape of mu: [1, 2, numComponents]
+   # shape of sig: [2, 2, numComponents]
 
- # calculate the density assigned to better efficiency values, for each lambda
-
- # shape of mu: [1, 2, numComponents]
- # shape of sig: [2, 2, numComponents]
-
- lambda = 0.9
- 
- phi = c(1, 0.9)
-
- numberOfClusters = length(weight)
-
- muEff = colSums(array(phi, dim=c(1, 2, numberOfClusters))  * mu, dims=2)
- sigEff = colSums(sig * aperm(array(phi, dim=c(2, 2, numberOfClusters)), perm=c(2,1,3)) * array(phi, dim=c(2, 2, numberOfClusters)), dim=2)
- 
- effGround = sum(phi * c(parsGround, surpGround))
- 
- quantiles = pnorm(effGround, mean=muEff, sd=sigEff)
- 
- totalQuantile = sum(quantiles * weight[[1]])
- cat(totalQuantile, "\n")
+   bestQuantile = 2.0  
+   bestLambda = NA
+   for(lambda in c(0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9)) {
+     phi = c(1, lambda)
+    
+     numberOfClusters = length(weight)
+    
+     muEff = colSums(array(phi, dim=c(1, 2, numberOfClusters))  * mu, dims=2)
+     sigEff = colSums(sig * aperm(array(phi, dim=c(2, 2, numberOfClusters)), perm=c(2,1,3)) * array(phi, dim=c(2, 2, numberOfClusters)), dim=2)
+     
+     effGround = sum(phi * c(parsGround, surpGround))
+     
+     quantiles = pnorm(effGround, mean=muEff, sd=sigEff)
+     
+     totalQuantile = sum(quantiles * weight)
+#     cat(lambda, totalQuantile, "\n")
+     if(totalQuantile <= bestQuantile) {
+	     bestQuantile = totalQuantile
+             bestLambda = lambda
+     }
+     if(totalQuantile > 1) {
+	     break
+   }
+   }
+   cat(bestLambda, bestQuantile, "\n")
+   bestLambdas = c(bestLambdas, bestLambda)
+   bestQuantiles = c(bestQuantiles, bestQuantile)
 }	
+
+
+plot(bestLambdas, 1-bestQuantiles)
+
+mean(bestLambdas)
+sd(bestLambdas)
+
+mean(1-bestQuantiles)
+
+ecdf(1-bestQuantiles)(0.5)
+
+
+hist(bestLambdas)
+
 
 
 
---
+++ results/plane/analyze_pareto_optimality/paretoByFamily.R
@@ -0,0 +1,54 @@
+data = read.csv("pareto-data.tsv")
+
+languages = read.csv("../../languages/languages-iso_codes.tsv")
+data  = merge(data, languages, by=c("Language"), all.x=TRUE)
+
+library(dplyr)
+library(tidyr)
+library(lme4)
+library(ggplot2)
+
+lambda = 0.9
+
+data = data %>% mutate(Better = (Pars + lambda*Surp) < (ParsGround + lambda*SurpGround))
+
+library(brms)
+
+model = brm(Better ~ (1|Family) + (1|Language), family="bernoulli", data=data)
+
+
+
+
+samples = posterior_samples(model, "b_Intercept")[,]
+posteriorOpposite = ecdf(samples)(0.0)
+
+for(family in unique(languages$Family)) {
+
+   samples_ = (samples + posterior_samples(model)[[paste("r_Family[",sub(" ", ".", family),",Intercept]", sep="")]])
+
+   plot = ggplot(data.frame(BetterThan = 1/(1+exp(samples_))), aes(x=BetterThan, y=..scaled..)) + geom_density(fill="blue")
+   plot = plot + xlim(0,1)
+   plot = plot + theme_classic()
+   plot = plot + theme_void()
+   plot = plot + theme(legend.position="none")
+   plot = plot + geom_segment(aes(x=0.5, xend=0.5, y=0, yend=1), linetype=2)
+#   plot = plot + theme(axis.line.x = element_line(colour = "black"))
+   ggsave(paste("figures/quantile_posterior_", lambda, "_", family, ".pdf", sep=""), plot=plot, height=1, width=2)
+
+
+
+}
+
+#lambda = 0.9
+summary(model)
+
+# Only Surp
+data = data %>% mutate(Better = (Surp) < (SurpGround))
+model = update(model, newdata=data)
+summary(model)
+
+# Only Pars
+data = data %>% mutate(Better = (Pars) < (ParsGround))
+model = update(model, newdata=data)
+summary(model)
+
