5fb980b00065f79dd47dd4d33f926994e8e83258
Michael Hahn <mhahn29@gmail.com>
2019-09-15 08:46:04 -0700

    quantile analysis

---
+++ results/plane/analyze_pareto_optimality/bayesianAnalysisIsoQuantile.R
@@ -0,0 +1,132 @@
+data = read.csv("pareto-data.tsv")
+
+
+library(dplyr)
+library(tidyr)
+
+language = "Estonian"
+#for(language in unique(data$Language)) {
+    
+    data2 = data %>% filter(Language == language)
+    
+    surp = data2$Surp
+    pars = data2$Pars
+    
+    surpGround = mean(data2$SurpGround)
+    parsGround = mean(data2$ParsGround)
+    
+    surpGround = (surpGround - mean(surp, na.rm=TRUE)) / sd(surp, na.rm=TRUE)
+    parsGround = (parsGround - mean(pars, na.rm=TRUE)) / sd(pars, na.rm=TRUE)
+    
+    surp = (surp - mean(surp, na.rm=TRUE)) / sd(surp, na.rm=TRUE)
+    pars = (pars - mean(pars, na.rm=TRUE)) / sd(pars, na.rm=TRUE)
+    
+    X = cbind(surp, pars)
+    X = X[!is.na(X[,1]),]
+    X = X[!is.na(X[,2]),]
+    
+    library(dirichletprocess)
+    dp = DirichletProcessMvnormal(X)
+    dp = Fit(dp, 2000)
+    
+    bestLambdas = c()
+    bestQuantiles = c()
+    
+    i = 1000
+    #for(i in (1000:2000)) {
+       weight = dp$weightsChain[[i]]
+       mu = dp$clusterParametersChain[[i]]$mu
+       sig = dp$clusterParametersChain[[i]]$sig
+    
+       numberOfClusters = length(weight)
+      
+
+       # calculate the density assigned to better efficiency values, for each lambda
+     
+       # create a grid of values at which to evaluate
+
+       parsGrid = ((0:50)-30)/10
+       surpGrid = ((0:50)-30)/10
+
+       parsGrid = array(array(parsGrid, dim=c(51, 51)), dim=c(51*51))
+       surpGrid = array(aperm(array(surpGrid, dim=c(51, 51)), c(2,1)), dim=c(51*51))
+       
+       
+       grid = cbind(parsGrid, surpGrid) # 51 x 2
+       # for each cluster, z-tansform the grids
+ 
+#       gridZ = array(grid, dim=c(51*51, 1, 2, numberOfClusters)) - array(mu, dim=c(1, 1, 2, numberOfClusters))
+       transform = array(apply(sig, 3, function(x) { return(solve(chol(x))) }), dim=c(2, 2, numberOfClusters))
+
+       gridZ = array(apply(array(grid, dim=c(51*51, 1, 2, numberOfClusters)), 1, function(x) { return((x-mu)) }), dim=c(numberOfClusters, 51*51, 1, 2))
+
+       total_gridZ_p = 0
+       for(q in (1:numberOfClusters)) {
+          gridZ_ = apply(gridZ[q,,,], 1, function(x) { return(transform[,,q] %*% x) })
+          gridZ_p = pnorm(gridZ_)
+          gridZ_p = gridZ_p[1,] * gridZ_p[2,]
+          total_gridZ_p = total_gridZ_p + weight[q] *     gridZ_p
+       }
+#       total_gridZ_p now has shape (51*51), containing the quantiles for all the grid elements
+# now can go back to the coordinates
+ 
+       # shape of mu: [1, 2, numComponents]
+       # shape of sig: [2, 2, numComponents]
+    
+    
+    #directly find the best lambda
+    
+       bestQuantile = 2.0  
+       bestLambda = NA
+       for(lambda_ in (0:19)) {
+         lambda = lambda_/20
+         phi = c(1, lambda)
+        
+        
+         muEff = colSums(array(phi, dim=c(1, 2, numberOfClusters))  * mu, dims=2)
+         sigEff = colSums(sig * aperm(array(phi, dim=c(2, 2, numberOfClusters)), perm=c(2,1,3)) * array(phi, dim=c(2, 2, numberOfClusters)), dim=2)
+         
+         effGround = sum(phi * c(parsGround, surpGround))
+         
+         quantiles = pnorm(effGround, mean=muEff, sd=sigEff)
+         
+         totalQuantile = sum(quantiles * weight)
+    #     cat(lambda, totalQuantile, "\n")
+         if(totalQuantile <= bestQuantile) {
+    	     bestQuantile = totalQuantile
+                 bestLambda = lambda
+         }
+         if(totalQuantile > 1) {
+    	     break
+       }
+       }
+       cat(bestLambda, bestQuantile, "\n")
+       bestLambdas = c(bestLambdas, bestLambda)
+       bestQuantiles = c(bestQuantiles, bestQuantile)
+#    }	
+    
+    
+#    write.csv(data.frame(bestLambdas=bestLambdas, bestQuantiles=bestQuantiles), file=paste("~/CS_SCR/posteriors/pareto-smooth/pareto-", language, sep=""))
+
+
+#}
+
+
+
+#plot(bestLambdas, 1-bestQuantiles)
+#
+#mean(bestLambdas)
+#sd(bestLambdas)
+#
+#mean(1-bestQuantiles)
+#
+#ecdf(1-bestQuantiles)(0.5)
+#
+#
+#hist(bestLambdas)
+#
+##analysis/efficiency-control-brms.R:cat(paste("dependency", "satisfiedFraction", "posteriorMean", "posteriorSD", "posteriorOpposite", sep="\t"), file="../output/results-prevalence-two-09-large.tsv", append=TRUE, sep="\n")
+#
+#
+
+
