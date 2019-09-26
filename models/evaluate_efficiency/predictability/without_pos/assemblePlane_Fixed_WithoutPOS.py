fd5d63b3133ca2d95e3e90de72ccd3ee447e140c
Michael Hahn <mhahn29@gmail.com>
2019-09-25 17:33:12 -0700

    code

--- models/evaluate_efficiency/predictability/without_pos/assemblePlane_Fixed_WithoutPOS.py
+++ models/evaluate_efficiency/predictability/without_pos/assemblePlane_Fixed_WithoutPOS.py
@@ -6,21 +6,24 @@ import sys
 
 
 dirs = []
-dirs.append("manual_output_funchead_two_coarse_final")
-dirs.append("manual_output_funchead_two_coarse_parser_final")
-dirs.append("manual_output_funchead_langmod_coarse_final")
-dirs.append("REAL_REAL")
-dirs.append("manual_output_funchead_langmod_coarse_tuning")
+dirs.append("manual_output_funchead_two_coarse_lambda09_best_large")
+dirs.append("manual_output_funchead_two_coarse_parser_best_balanced")
+dirs.append("manual_output_funchead_langmod_coarse_best_balanced")
 dirs.append("manual_output_funchead_RANDOM")
-dirs.append("manual_output_funchead_coarse_depl")
+dirs.append("manual_output_funchead_RANDOM2")
+dirs.append("manual_output_funchead_RANDOM3")
+dirs.append("manual_output_funchead_RANDOM4")
+dirs.append("manual_output_funchead_RANDOM5")
+#dirs.append("manual_output_funchead_coarse_depl")
 dirs.append("manual_output_funchead_ground_coarse_final")
-dirs.append("manual_output_funchead_RLR")
+#dirs.append("manual_output_funchead_RLR")
+dirs.append("REAL_REAL")
 
 outPath = "../../../../grammars/plane/controls/plane-fixed-withoutPOS.tsv"
 with open(outPath, "w") as outFile:
   print >> outFile, "Language\tModel\tType\tSurp"
   for BASE_DIR in dirs:
-    
+    found = 0
     
     languages = ["Hindi", "Swedish", "German", "Urdu", "English", "Spanish", "Chinese", "Slovenian", "Estonian", "Norwegian", "Serbian", "Croatian", "Finnish", "Portuguese", "Catalan", "Russian", "Arabic", "Czech", "Japanese", "French", "Latvian", "Basque", "Danish", "Dutch", "Ukrainian", "Gothic", "Hebrew", "Hungarian", "Latin", "Persian", "Bulgarian", "Romanian", "Indonesian", "Greek", "Turkish", "Slovak", "Belarusian", "Galician", "Italian", "Lithuanian", "Polish", "Vietnamese", "Korean", "Tamil", "Irish", "Marathi", "Afrikaans", "Telugu", "Coptic", "Ancient_Greek", "Old_Church_Slavonic"]
     assert len(languages) == 51, len(languages)
@@ -55,7 +58,7 @@ with open(outPath, "w") as outFile:
          elif "RANDOM" in BASE_DIR:
              parts = models[i].split("_")
          #    print(parts)
-             if len(parts) < 4 or parts[-3] != "RANDOM" or parts[-2] != "model":
+             if len(parts) < 4 or parts[-3] not in ["RANDOM", "RANDOM2", "RANDOM3", "RANDOM4", "RANDOM5"] or parts[-2] != "model":
                 print(models[i])
                 continue
              if parts[-1].endswith(".tsv"):
@@ -117,9 +120,13 @@ with open(outPath, "w") as outFile:
     #              print(data)
                   data = data[1:]
                   if len(data[0]) == 1:
+                     print("Unfinished model", name, BASE_DIR)
                      continue
                   if float(data[0][-1]) < float(data[0][-2]):
+                     print("Unfinished model", name, BASE_DIR)
                      continue
+                  found+=1
                   print >> outFile, ("\t".join([language, model, BASE_DIR, data[1][-2]]))
+    print([BASE_DIR, found])
 
 print(outPath)
