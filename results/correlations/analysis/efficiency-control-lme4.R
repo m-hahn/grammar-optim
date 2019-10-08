6a41f45304a2f43fb0d36e8636f812c1c5a89edd
Michael Hahn <mhahn29@gmail.com>
2019-10-08 15:37:50 -0400

    code

--- results/correlations/analysis/efficiency-control-lme4.R
+++ results/correlations/analysis/efficiency-control-lme4.R
@@ -16,13 +16,13 @@ data  = merge(data, languages, by=c("Language"), all.x=TRUE)
 dependency = "nmod"
 dependencies = c("acl", "aux", "lifted_case", "lifted_cop", "lifted_mark", "nmod", "obl", "xcomp")
 objs = data %>% filter(CoarseDependency == "obj") %>% mutate(obj = pmax(0, sign(DH_Weight))) %>% select(obj, Language, FileName)
+
 data = data %>% filter((CoarseDependency %in% dependencies))
 data = merge(data, objs, by=c("Language", "FileName"))
 data = data %>% mutate(dir = pmax(0, sign(DH_Weight)))
 data = data %>% mutate(dir = ifelse(DH_Weight == 0, NA, dir))
 data$agree = (data$dir == data$obj)
 data = unique(data %>% select(Family, Language, FileName, CoarseDependency, agree)) %>% spread(CoarseDependency, agree)
-
 
 library(lme4)
 
@@ -38,4 +38,9 @@ for(dependency in dependencies) {
 sink()
 
 
+
+##################################
+
+#data = merge(data, objs, by=c("Language", "FileName"))
+#summary(glmer(paste(dependency, " ~ obj + (1|Family) + (1|Language)"), family="binomial", data=data))
 
