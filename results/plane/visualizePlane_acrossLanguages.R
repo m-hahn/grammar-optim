4b90c3304bb91ac3a4ab786ec7ca8112f9505d1f
Michael Hahn <mhahn29@gmail.com>
2019-10-17 17:45:26 -0400

    code

---
+++ results/plane/visualizePlane_acrossLanguages.R
@@ -0,0 +1,196 @@
+# per language
+
+
+library(lme4)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+dataS = read.csv("../../grammars/plane/plane-fixed.tsv", sep="\t") %>% mutate(Model = as.character(Model))
+dataS2 = read.csv("../../grammars/plane/plane-fixed-best.tsv", sep="\t") %>% mutate(Model = as.character(Model))
+dataS3 = read.csv("../../grammars/plane/plane-fixed-best-large.tsv", sep="\t") %>% mutate(Model = as.character(Model)) %>% mutate(FullSurp = NULL)
+dataS4 = read.csv("../../grammars/plane/plane-fixed-random2.tsv", sep="\t") %>% mutate(Model = as.character(Model)) %>% mutate(FullSurp = NULL)
+dataS5 = read.csv("../../grammars/plane/plane-fixed-random3.tsv", sep="\t") %>% mutate(Model = as.character(Model)) %>% mutate(FullSurp = NULL)
+dataS6 = read.csv("../../grammars/plane/plane-fixed-random4.tsv", sep="\t") %>% mutate(Model = as.character(Model)) %>% mutate(FullSurp = NULL)
+dataS7 = read.csv("../../grammars/plane/plane-fixed-random5.tsv", sep="\t") %>% mutate(Model = as.character(Model)) %>% mutate(FullSurp = NULL)
+dataS = rbind(dataS, dataS2, dataS3, dataS4, dataS5, dataS6, dataS7)
+dataP = read.csv("../../grammars/plane/plane-parse-unified.tsv", sep="\t") %>% mutate(Model = as.character(Model))
+dataS = dataS %>% group_by(Language, Type, Model) %>% summarise(Surprisal = mean(Surp, na.rm=TRUE))
+dataP = dataP %>% group_by(Language, Type, Model) %>% summarise(Pars = mean(Pars, na.rm=TRUE))
+dataS = as.data.frame(dataS)
+dataP = as.data.frame(dataP)
+library(lme4)
+summary(lmer(Surprisal ~ Type + (1|Language), data=dataS %>% filter(grepl("langm", Type))))
+summary(lmer(Surprisal ~ Type + (1|Language), data=dataS %>% filter(grepl("RL", Type) | grepl("ground", Type))))
+dataS = dataS %>% mutate(Type = as.character(Type))
+dataP = dataP %>% mutate(Type = as.character(Type))
+dataS = dataS %>% mutate(Model = as.character(Model))
+dataP = dataP %>% mutate(Model = as.character(Model))
+dataS = dataS %>% mutate(Language = as.character(Language))
+dataP = dataP %>% mutate(Language = as.character(Language))
+data = merge(dataS, dataP, by=c("Language", "Model", "Type"), all.x=TRUE, all.y=TRUE) %>% filter(Language %in% c("English", "Japanese"))
+
+# Transferred manually from
+# ~/scr/deps//language_modeling_coarse_plane_fixed_counter/
+# ../../../../raw-results/parsing-upos2-counter/
+data2 = data.frame(Language = c("English", "Japanese"), Model = c("3679914", "7520227"), Type = c("xground_other", "xground_other"), Surprisal = c(6.550901074296865, 6.273405530411384), Pars = c(0.7777210038622745, 0.7052601910851886))
+data = rbind(data, data2)
+
+data = data %>% mutate(Type = ifelse(Type == "manual_output_funchead_RANDOM2", "manual_output_funchead_RANDOM", as.character(Type)))
+data = data %>% mutate(Type = ifelse(Type == "manual_output_funchead_RANDOM3", "manual_output_funchead_RANDOM", as.character(Type)))
+data = data %>% mutate(Type = ifelse(Type == "manual_output_funchead_RANDOM4", "manual_output_funchead_RANDOM", as.character(Type)))
+data = data %>% mutate(Type = ifelse(Type == "manual_output_funchead_RANDOM5", "manual_output_funchead_RANDOM", as.character(Type)))
+
+
+
+
+
+dataComp = data
+dataCompBaseline = dataComp %>% filter(grepl("RANDOM", Type))
+dataCompGround = dataComp %>% filter(Type == "manual_output_funchead_ground_coarse_final") %>% select(Language, Surprisal, Pars) %>% rename(SurprisalGround = Surprisal) %>% rename(ParsGround = Pars) %>% mutate(EffGround = ParsGround + 0.9*SurprisalGround) %>% group_by(Language)
+dataComp = merge(dataCompBaseline, dataCompGround, by=c("Language"))
+
+dataComp$Eff = dataComp$Pars + 0.9*dataComp$Surprisal
+
+u = dataComp %>% group_by(Language) %>% summarise(BetterSurprisal = sum(Surprisal > SurprisalGround, na.rm=TRUE), WorseSurprisal = sum(Surprisal <= SurprisalGround, na.rm=TRUE), TotalSurprisal = BetterSurprisal+WorseSurprisal, BetterFracSurprisal = BetterSurprisal/TotalSurprisal, BetterPars = sum(Pars > ParsGround, na.rm=TRUE), WorsePars = sum(Pars <= ParsGround, na.rm=TRUE), TotalPars = BetterPars+WorsePars, BetterFracPars = BetterPars/TotalPars, BetterEff = sum(Eff > EffGround, na.rm=TRUE), WorseEff = sum(Eff <= EffGround, na.rm=TRUE), TotalEff = BetterEff+WorseEff, BetterFracEff = BetterEff/TotalEff )
+
+
+
+
+
+data = data %>% mutate(Two = 0.9*Surprisal+Pars)
+
+data2 = rbind(data)
+data2 = data2 %>% group_by(Language, Type) %>% summarise(Surprisal=mean(Surprisal, na.rm=TRUE), Pars=mean(Pars, na.rm=TRUE)) %>% group_by(Language) %>% mutate(MeanSurprisal = mean(Surprisal, na.rm=TRUE), SDSurprisal = sd(Surprisal, na.rm=TRUE)) %>% mutate(MeanPars = mean(Pars, na.rm=TRUE), SDPars = sd(Pars, na.rm=TRUE))
+
+dataPBest = data %>% filter(Type == "manual_output_funchead_two_coarse_parser_best_balanced") %>% group_by(Language) %>% summarise(Pars = min(Pars))
+data2Best = data %>% filter(Type == "manual_output_funchead_two_coarse_lambda09_best_large") %>% group_by(Language) %>% summarise(Two = min(Two, na.rm=TRUE))
+dataSBest = data %>% filter(Type == "manual_output_funchead_langmod_coarse_best_balanced") %>% group_by(Language) %>% summarise(Surprisal = min(Surprisal, na.rm=TRUE))
+
+dataP = data %>% filter(Type == "manual_output_funchead_two_coarse_parser_best_balanced")
+data2 = data %>% filter(Type == "manual_output_funchead_two_coarse_lambda09_best_large")
+dataS = data %>% filter(Type == "manual_output_funchead_langmod_coarse_best_balanced")
+
+dataP = merge(dataP, dataPBest, by=c("Language", "Pars"))
+data2 = merge(data2, data2Best, by=c("Language", "Two"))
+dataS = merge(dataS, dataSBest, by=c("Language", "Surprisal"))
+
+
+dataRandom = data %>% filter(grepl("RANDOM", Type))
+dataGround = data %>% filter(grepl("ground", Type)) #%>% group_by(Language) %>% summarise(Surprisal = mean(Surprisal), Pars = mean(Pars))
+
+
+D = dataGround %>% select(Language, Type, Model)
+
+
+
+
+
+
+data = rbind(dataP, data2)
+data = rbind(data, dataS)
+data = rbind(data, dataRandom)
+data = rbind(data, dataGround)
+
+
+
+dataMean = data %>% group_by(Language, Type) %>% summarise(Surprisal=mean(Surprisal, na.rm=TRUE)) %>% group_by(Language) %>% summarise(MeanSurprisal = mean(Surprisal, na.rm=TRUE), SDSurprisal = sd(Surprisal, na.rm=TRUE)+0.0001)
+data = merge(data, dataMean, by=c("Language"))
+dataMean = data %>% group_by(Language, Type) %>% summarise(Pars=mean(Pars, na.rm=TRUE)) %>% group_by(Language) %>% summarise(MeanPars = mean(Pars, na.rm=TRUE), SDPars = sd(Pars, na.rm=TRUE)+0.0001)
+data = merge(data, dataMean, by=c("Language"))
+
+dataMean = data %>% filter(grepl("RANDOM", Type)) %>% group_by(Language) %>% summarise(MeanSurprisalRand = mean(Surprisal, na.rm=TRUE), SDSurprisalRand = sd(Surprisal, na.rm=TRUE)+0.0001)
+data = merge(data, dataMean, by=c("Language"))
+dataMean = data %>% filter(grepl("RANDOM", Type)) %>% group_by(Language) %>% summarise(MeanParsRand = mean(Pars, na.rm=TRUE), SDParsRand = sd(Pars, na.rm=TRUE)+0.0001)
+data = merge(data, dataMean, by=c("Language"))
+
+
+
+D = data %>% filter(Type == "manual_output_funchead_two_coarse_lambda09_best_large") %>% select(Language, Type, Model, Pars, Surprisal)
+D = data %>% filter(Type == "manual_output_funchead_langmod_coarse_best_balanced") %>% select(Language, Type, Model, Surprisal)
+D = data %>% filter(Type == "manual_output_funchead_two_coarse_parser_best_balanced") %>% select(Language, Type, Model, Pars)
+
+
+
+
+
+data = data %>% mutate(Pars_z = (Pars-MeanPars)/SDPars, Surprisal_z=(Surprisal-MeanSurprisal)/SDSurprisal)
+
+subData = data %>% filter(Type %in% c("manual_output_funchead_two_coarse_parser_best_balanced", "manual_output_funchead_two_coarse_lambda09_best_large", "manual_output_funchead_langmod_coarse_best_balanced"))
+
+
+#######################################
+
+subData = subData[order(subData$Type),]
+
+Step = c()
+Surprisal_z = c()
+Pars_z = c()
+Language = c()
+for(language in unique(subData$Language)) {
+   u = subData %>% filter(Language == language)
+
+   # Pareto hull
+
+   pred = min(u$Surprisal_z)
+   pars = (u %>% filter(Type == "manual_output_funchead_langmod_coarse_best_balanced"))$Pars_z[1]
+
+   Surprisal_z = c(Surprisal_z, pred)
+   Pars_z = c(Pars_z, pars)
+   Language = c(Language, language)
+   Step = c(Step, 1)
+
+   pred = (u %>% filter(Type == "manual_output_funchead_two_coarse_lambda09_best_large"))$Surprisal_z[1]
+   pars = (u %>% filter(Type == "manual_output_funchead_two_coarse_lambda09_best_large"))$Pars_z[1]
+
+   Surprisal_z = c(Surprisal_z, pred)
+   Pars_z = c(Pars_z, pars)
+   Language = c(Language, language)
+   Step = c(Step, 2)
+
+   pred = (u %>% filter(Type == "manual_output_funchead_two_coarse_parser_best_balanced"))$Surprisal_z[1]
+   pars = min(u$Pars_z)
+
+   Surprisal_z = c(Surprisal_z, pred)
+   Pars_z = c(Pars_z, pars)
+   Language = c(Language, language)
+   Step = c(Step, 3)
+
+}
+
+subData = data.frame(Language=Language, Surprisal_z=Surprisal_z, Pars_z=Pars_z, Step=Step)
+subData$Type = "Pareto"
+
+
+
+corpusSize = read.csv("../corpus-size/corpus-sizes.tsv", sep="\t")
+languagesOrdered = corpusSize$language[order(-corpusSize$sents_train)]
+
+data$Language = factor(data$Language, levels=languagesOrdered)
+subData$Language = factor(subData$Language, levels=languagesOrdered)
+
+
+
+plot = ggplot(data %>% filter(Type %in% c("manual_output_funchead_RANDOM")) %>% filter(Surprisal_z < 3), aes(x=-Pars_z, y=-Surprisal_z, color=Type, group=Type))
+plot = plot + geom_point()
+plot = plot + geom_path(data=subData, aes(x=-Pars_z, y=-Surprisal_z, group=1), size=1.5)
+plot = plot + geom_point(data=data %>% filter(Type %in% c("manual_output_funchead_ground_coarse_final")) %>% filter(Surprisal_z < 3), shape=4, size=1.5, stroke=2)
+plot = plot + geom_point(data=data %>% filter(Type %in% c("xground_other")) %>% filter(Surprisal_z < 3), shape=5, size=1.5, stroke=2)
+plot = plot + facet_wrap(~Language, scales="free")
+plot = plot + theme_bw()
+plot = plot + scale_x_continuous(name="Parseability") + scale_y_continuous(name="Predictability")
+plot = plot + theme(legend.title = element_blank())  
+plot = plot + guides(color=guide_legend(nrow=2,ncol=4,byrow=TRUE)) 
+plot = plot + theme(legend.title = element_blank(), legend.position="bottom")
+plot = plot + theme(axis.title.x = element_text(size=17))
+plot = plot + theme(axis.title.y = element_text(size=17))
+plot = plot + theme(legend.text = element_text(size=12))
+plot = plot + theme(legend.margin=margin(t = 0, unit='cm'))
+plot = plot + theme(legend.position = "none")
+ggsave(plot, file="pareto-plane-other-language.pdf", width=8, height=4)
+
+
+
+
+
