2e622ddd215603710c4bfce59fec88784f80abf8
Michael Hahn <mhahn29@gmail.com>
2019-10-24 20:07:36 -0400

    cleaning

---
+++ results/plane/pureUD/analyze_pareto_optimality/plotOptimalityByLambda.R
@@ -0,0 +1,41 @@
+data = read.csv("optimality-by-lambda.tsv", sep="\t")
+
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+
+plot = ggplot(data, aes(x=Lambda, y=Quantile)) + geom_line() + facet_wrap(~Language)
+
+cBoundUpper = c()
+cBoundLower = c()
+pValue = c()
+for(i in (1:nrow(data))) {
+   quantile = data$Quantile[[i]]
+   samples = data$Samples[[i]]
+   test = binom.test(round(quantile*samples), round(samples), alternative="two.sided", conf.level=1-0.05)
+   cBoundUpper = c(cBoundUpper, test$conf.int[[2]])
+   cBoundLower = c(cBoundLower, test$conf.int[[1]])
+   pValue = c(pValue, test$p.value*3)
+}
+data$UpperBound = cBoundUpper
+data$LowerBound = cBoundLower
+data$pValue = pValue
+
+
+plot = ggplot(data, aes(x=Lambda, y=Quantile)) + geom_line() + geom_line(aes(x=Lambda,y=LowerBound)) + facet_wrap(~Language)
+
+
+
+corpusSize = read.csv("../../../corpus-size/corpus-sizes.tsv", sep="\t")
+languagesOrdered = corpusSize$language[order(-corpusSize$sents_train)]
+
+data$Language = factor(data$Language, levels=languagesOrdered)
+
+
+
+plot = ggplot(data, aes(x=Lambda, y=Quantile)) + geom_line() + geom_line(aes(x=Lambda,y=UpperBound), linetype="dotted") + geom_line(aes(x=Lambda,y=LowerBound), linetype="dotted") + facet_wrap(~Language)
+
+
+ggsave(plot, file="figures/quantileByLambda.pdf")
+
+
